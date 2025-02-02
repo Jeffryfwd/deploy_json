d4aec76af4f0fda1a09584b442f1e7e18df9836f
Christian-Jordy05 <yordymar224@gmail.com>
2025-02-01 23:42:07 -0600

    jjkkjl

--- api/db.js
+++ api/db.js
@@ -1,10 +1,9 @@
-// api/db.js
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 export default async function handler(req, res) {
   try {
-    const filePath = path.resolve('./db.json'); // Ajusta la ruta según donde esté tu db.json
+    const filePath = path.resolve('./db.json');
     const jsonData = fs.readFileSync(filePath, 'utf-8');
     const data = JSON.parse(jsonData);
     res.status(200).json(data);
